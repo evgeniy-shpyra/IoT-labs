8d830f3eb420a150d2c10a2b43ec038c1d7a1c9d
Evgeniy <56407183+evgeniy-shpyra@users.noreply.github.com>
2024-03-09 22:54:50 +0200

    lab 3: init, db

---
+++ lab3/src/db/index.js
@@ -0,0 +1,44 @@
+import { Sequelize } from 'sequelize'
+import Agent from './models/agent.js'
+import agentRepo from './repositories/agent.js'
+
+const initRepo = (sequelize) => ({
+  agent: agentRepo(sequelize),
+})
+
+const initModels = async (sequelize) => {
+  Agent(sequelize)
+  // await sequelize.sync({ force: true })
+  await sequelize.sync({ alter: true })
+}
+
+const DB = ({ user, password, host, name, port }) => {
+
+  const sequelize = new Sequelize(
+    `postgres://${user}:${password}@${host}:${port}/${name}`
+  )
+  return {
+    start: async () => {
+      try {
+        await sequelize.authenticate()
+        console.log('Db has been started')
+      } catch (error) {
+        console.error(`Couldn't start db:`, error)
+        process.exit(1)
+      }
+      await initModels(sequelize)
+      const repositories = await initRepo(sequelize)
+      return repositories
+    },
+
+    stop: async () => {
+      try {
+        await sequelize.close()
+        console.log('Db has been closed')
+      } catch (err) {
+        console.log('An error occurred while closing the db', err)
+      }
+    },
+  }
+}
+export default DB
---
+++ lab3/src/db/repositories/agent.js
@@ -0,0 +1,41 @@
+const repo = (sequelize) => {
+  const Agent = sequelize.models.processed_agent_data
+
+  return {
+    create: async (payload) => {
+      await Agent.create(payload)
+      return true
+    },
+    getAll: async () => {
+      const data = await Agent.findAll({ raw: true })
+      return data
+    },
+    update: async (data, id) => {
+      const response = await Agent.update(data, {
+        where: {
+          id,
+        },
+      })
+      return Boolean(response[0])
+    },
+    getOne: async (id) => {
+      const data = await Agent.findOne({
+        where: {
+          id,
+        },
+      })
+
+      return data
+    },
+    delete: async (id) => {
+      const response = await Agent.destroy({
+        where: {
+          id,
+        },
+      })
+      return Boolean(response)
+    },
+  }
+}
+
+export default repo
